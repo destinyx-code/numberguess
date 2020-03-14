53f196d60a18cdec8b1fe35bf9e41e2d7ef618b6
Destiny <ryderstanley2007@gmail.com>
2020-03-13 19:37:44 -0600

    initial code commit

---
+++ numberguess.js
@@ -0,0 +1,11 @@
+  // Asks for input
+	var userInput = console.ReadLine('Choose any number!');
+		// The correct number variable
+		var correctNumber = 7;
+			// If statement
+			if (userInput == 7) {
+				console.log('Correct!');
+				}
+			else {
+				console.log('Wrong!');
+				}                                                                                                                           
